22facd252ddd14cc6ebc6c1154ec138184ff9232
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-04-15 09:55:13 +0000

    Feat(Task 0): Create a class BasicCache that inherits from BaseCaching and is a caching system

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Basic dictionary
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache is a caching system that does not enforce any limit.
+    """
+    
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return
+        return self.cache_data.get(key)
