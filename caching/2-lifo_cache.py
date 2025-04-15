2c8c9c368339c5a3d116bf311d0eb41004bf8a3a
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-04-15 14:00:59 +0000

    fix: if key exist with del it and replace at the end

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-""" Basic dictionary
-"""
+"""LIFOCache implementation with correct behavior"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ caching system
-    """
 
     def __init__(self):
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """ LIFO
+        """
+        Add or update cache, discard the last added item if over capacity.
         """
         if key is None or item is None:
             return
 
-        if key not in self.cache_data:
-            self.order.append(key)
+        if key in self.cache_data:
+            self.order.remove(key)
+
+        self.order.append(key)
         self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            removed = self.order.pop()
+            removed = self.order.pop(-2)
             del self.cache_data[removed]
             print(f"DISCARD: {removed}")
 
     def get(self, key):
-        """ get methode
-        """
+        """Get cache item by key."""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
