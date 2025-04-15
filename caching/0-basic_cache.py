6acce73fa6a21e54c3aadddc67240bd2ecf451d9
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-04-15 10:16:05 +0000

    fix: try to be ok with checker

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -10,11 +10,19 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
+        """ Add an item in the cache
+        :param key: The key for the cache entry.
+        :param item: The value associated with the key.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Get an item by key
+        :param key: the key to be retrieved in the cache
+        :returns: The value associated to the key, None otherwise
+        """
         if key is None or key not in self.cache_data:
             return
         return self.cache_data.get(key)
