f087e3edaf1313037b758e5c78822c7501ad912e
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-04-15 10:10:59 +0000

    fix: pycodestyle

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,10 +4,11 @@
 
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BaseCaching):
     """ BasicCache is a caching system that does not enforce any limit.
     """
-    
+
     def put(self, key, item):
         if key is None or item is None:
             return
