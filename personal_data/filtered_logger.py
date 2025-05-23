387f82cbcf9576105c5c8b6154ba3e74355b5ea8
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-05-23 11:45:59 +0000

    fix: less 5 lines

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,12 +6,6 @@ Write a function called filter_datum that returns the log message obfuscated
 
 
 def filter_datum(fields, redaction, message, separator):
-    """
-    fields: a list of strings representing all fields to obfuscate
-    redaction: a string representing by what the field will be obfuscated
-    message: a string representing the log line
-    separator: a string representing by which character
-    is separating all fields in the log line (message)
-    """
+    """ task 0 """
     pattern = r'(' + '|'.join(fields) + r')=[^' + re.escape(separator) + r']*'
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
