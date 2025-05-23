bd50a2bf4ffc2bafc66d5fa1b9f0682edd81e1c6
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-05-23 11:51:20 +0000

    new test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import re
-"""
-Write a function called filter_datum that returns the log message obfuscated
-"""
+""" Task 0 """
 
 
 def filter_datum(fields, redaction, message, separator):
-    """ task 0 """
-    pattern = r'(' + '|'.join(fields) + r')=[^' + re.escape(separator) + r']*'
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+    """ Task 0 """
+    patt = rf'({"|".join(map(re.escape, fields))})=[^{re.escape(separator)}]*'
+    return re.sub(patt, lambda m: f"{m.group(1)}={redaction}", message)
