ea958b260f5f77830a1eb16f5f9626eba9c001f8
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-06-13 09:25:11 +0000

    feat: add Error handler(401): Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint() -> str:
+    """ Endpoint that triggers 401 error """
+    abort(401)
