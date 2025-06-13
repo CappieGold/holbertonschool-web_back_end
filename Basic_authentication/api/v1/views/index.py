7098318b8c8a8efc2969bd395adf800e48fa60ff
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-06-13 09:27:43 +0000

    fix: pycodestyle

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_endpoint() -> str:
     """ Endpoint that triggers 401 error """
