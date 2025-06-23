23f6324b40f6512f1196414c7a8fe3c11d8de4a8
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-06-23 11:17:45 +0000

    feat: Create a class SessionAuth that inherits from Auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Session Auth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session authentication class
+    """
+    pass
