5476db074aab9f8b88fa07eecb7aafe5c963a05d
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-06-24 13:53:25 +0000

    feat: Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,8 +2,11 @@
 """
 Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -14,6 +17,24 @@ def bienvenue():
     GET / route that returns a welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """
+    POST /users route to register a new user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({
+            "email": email,
+            "message": "user created"
+        })
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
