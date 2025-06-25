838e0071a3f26243439facacb1cfc2d36168db52
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-06-25 11:07:43 +0000

    feat: Find user by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -67,3 +67,13 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Get a user from the session ID.
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
