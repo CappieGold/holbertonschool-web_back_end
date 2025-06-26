8e9dbbb83cd305c9f964d43974db8acdb405b8f4
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-06-26 09:26:55 +0000

    feat: Get reset password token

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -92,5 +92,22 @@ def profile():
         return flask.abort(403)
 
 
+@app.route("/reset_password", methods=['POST'])
+def get_reset_password_token():
+    """
+    POST /reset_password route to get reset password token
+    """
+    email = request.form.get('email')
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({
+            "email": email,
+            "reset_token": reset_token
+        }), 200
+    except ValueError:
+        return ValueError, 403
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
