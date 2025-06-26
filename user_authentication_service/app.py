7f02f47402016d902f6ae35e83cc63a7ab6b9814
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-06-26 09:29:25 +0000

    fix: return just 403

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -106,7 +106,7 @@ def get_reset_password_token():
             "reset_token": reset_token
         }), 200
     except ValueError:
-        return ValueError, 403
+        return 403
 
 
 if __name__ == "__main__":
