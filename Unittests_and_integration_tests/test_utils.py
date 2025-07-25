8566b22119cf81b4839999759d09569b3f9376bb
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-07-25 10:31:56 +0000

    feat: Task 1 Parameterize a unit test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test que access_nested_map retourne la valeur attendue"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test que access_nested_map lève une KeyError pour les chemins invalides
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
 
 if __name__ == '__main__':
     unittest.main()
