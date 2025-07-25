b255eded547090018225896280de9061f4eb4387
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-07-25 13:50:35 +0000

    feat: task 3 Parametrize templates

---
+++ i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Application Flask avec traductions
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config:
+    """Classe de configuration pour l'application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel()
+
+
+def get_locale():
+    """
+    Détermine la meilleure langue à utiliser pour l'utilisateur
+    basée sur les langues acceptées par le navigateur
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """Route principale qui affiche la page d'accueil"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
