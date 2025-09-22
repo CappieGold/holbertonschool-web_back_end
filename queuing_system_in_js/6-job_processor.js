c482859122bfda26b2ce4f193ff79a138bc97ccc
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-09-22 10:58:18 +0000

    feat: task 7: Create the Job processor

---
+++ queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,17 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+queue.process('push_notification_code', 1, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  try {
+    sendNotification(phoneNumber, message);
+    done();
+  } catch (err) {
+    done(err);
+  }
+});
