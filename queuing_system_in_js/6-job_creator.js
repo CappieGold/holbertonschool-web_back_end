4d48a141156d26ccfb2adcd74465e3ca149b6ad0
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-09-22 10:59:04 +0000

    fix: use same number as task

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -3,7 +3,7 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: '0674387559',
+  phoneNumber: '4153518780',
   message: 'This is the code to verify your account',
 };
 
